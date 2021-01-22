c35b95fd5e43525ded2fd5652ef1b9f315dc33d2
Daniel Gamboa <danielgamboa10@gmail.com>
2021-01-22 10:53:43 -0600

    Fix bug in base url and add axios get call to SWAPI in App to set the characters state

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import { BASE_URL } from './constants/base_url'
 import './App.css';
 
 const App = () => {
@@ -8,6 +10,22 @@ const App = () => {
   // Fetch characters from the API in an effect hook. Remember, anytime you have a 
   // side effect in a component, you want to think about which state and/or props it should
   // sync up with, if any.
+  const [characters, setCharacters] = useState('');
+
+  useEffect(() => {
+    const fetchCharacters = () => {
+      axios.get(`${BASE_URL}people/`)
+        .then(res => {
+          console.table(res.data);
+          setCharacters(res.data.results)
+        })
+        .catch(err => {
+          console.log(err);
+          debugger
+        });
+    }
+    fetchCharacters();
+  }, [])
 
   return (
     <div className="App">
